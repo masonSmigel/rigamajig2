521827c93ccd16608a6f4b4c502b6247f37fd222
masonSmigel <mgsmigel@gmail.com>
2021-12-07 23:43:50 -0500

    added load and save functions for joints and controls

--- scripts/rigamajig2/maya/rig/builder.py
+++ scripts/rigamajig2/maya/rig/builder.py
@@ -9,6 +9,7 @@ import rigamajig2.shared.common as common
 import rigamajig2.shared.runScript as runScript
 import rigamajig2.maya.data.abstract_data as abstract_data
 import rigamajig2.maya.file as file
+import rigamajig2.maya.meta as meta
 from rigamajig2.maya.cmpts import *
 
 import logging
@@ -26,6 +27,7 @@ POST_SCRIPT_PATH = 'post_script'
 PUB_SCRIPT_PATH = 'pub_script'
 
 # RIG FILE KEYS
+MODEL_FILE = "model_file"
 SKELETON_FILE = "skeleton_file"
 SKELETON_POS = "skeleton_pos"
 CONTROL_SHAPES = "control_shapes"
@@ -46,8 +48,6 @@ class Builder(object):
 
         # varibles we need
         self.top_skeleton_nodes = list()
-
-        logger.info('Initalize Rig Builder: {0}'.format(self.path))
 
     def getComponents(self):
         path = CMPT_PATH
@@ -70,24 +70,65 @@ class Builder(object):
 
     # RIG BUILD STEPS
     def import_model(self, path=None):
-        pass
+        if path and os.path.exists(path=path):
+            model = file.import_(path, ns=None)
+        elif os.path.exists(self.absPath(self.get_rig_data(self.rig_file, MODEL_FILE))):
+            model = file.import_(self.absPath(self.get_rig_data(self.rig_file, MODEL_FILE)), ns=None)
+        else:
+            return
 
-    def import_skeleton(self):
-        import rigamajig2.maya.data.node_data as node_data
-
-        if os.path.exists(self.absPath(self.read_data(SKELETON_FILE))):
-            nodes = file.import_(self.absPath(self.read_data(SKELETON_FILE)), ns=None)
-            joint_data = node_data.NodeData()
-            # joint_data.read(self.absPath(self.read_data(SKELETON_POS)))
-            # joint_data.applyData()
+    def import_skeleton(self, path=None):
+        if path and os.path.exists(path=path):
+            nodes = file.import_(path, ns=None)
+        elif os.path.exists(self.absPath(self.get_rig_data(self.rig_file, SKELETON_FILE))):
+            nodes = file.import_(self.absPath(self.get_rig_data(self.rig_file, SKELETON_FILE)), ns=None)
         else:
             return
 
         # get top level nodes in the skeleton
         for node in cmds.ls(nodes, l=True, type='transform'):
             if not len(node.split('|')) > 2:
-                self.top_skeleton_nodes.append(node)
+                # self.top_skeleton_nodes.append(node)
+                meta.tag(node, 'skeleton_root')
+
         logger.info("skeleton imported")
+
+    def load_joint_positions(self, path=None):
+        import rigamajig2.maya.data.joint_data as joint_data
+        if path and os.path.exists(path=path):
+            joint_pos_path = path
+        elif os.path.exists(self.absPath(self.get_rig_data(self.rig_file, SKELETON_POS))):
+            joint_pos_path = self.absPath(self.get_rig_data(self.rig_file, SKELETON_POS))
+        else:
+            return
+
+        if os.path.exists(joint_pos_path):
+            data_obj = joint_data.JointData()
+            data_obj.read(joint_pos_path)
+            data_obj.applyData(data_obj.getData().keys())
+            logger.info("Joint positions loaded from: {}".format(joint_pos_path))
+
+    def save_joint_positions(self, path=None):
+        import rigamajig2.maya.data.joint_data as joint_data
+
+        if path:
+            joint_pos_path = path
+        elif self.get_rig_data(self.rig_file, SKELETON_POS):
+            joint_pos_path = self.absPath(self.get_rig_data(self.rig_file, SKELETON_POS))
+        else:
+            return
+
+        # find all skeleton roots and get the positions of their children
+        skeleton_roots = common.toList(meta.getTagged('skeleton_root'))
+        if len(skeleton_roots) > 0:
+            data_obj = joint_data.JointData()
+            for root in skeleton_roots:
+                logger.debug("Gathering data of joints under skeleton root: {}".format(root))
+                data_obj.gatherDataIterate(cmds.listRelatives(root, allDescendents=True, type='joint'))
+            data_obj.write(joint_pos_path)
+            logger.info("Joint positions saved to: {}".format(joint_pos_path))
+        else:
+            raise RuntimeError("the root joint {} does not exists".format(skeleton_roots))
 
     def initalize(self):
         """Initalize rig (this is where the user can make changes)"""
@@ -107,7 +148,8 @@ class Builder(object):
 
         # parent the bind joints to the bind group. if one exists
         if cmds.objExists('bind'):
-            cmds.parent(self.top_skeleton_nodes, 'bind')
+            top_skeleton_nodes = meta.getTagged('skeleton_root')
+            cmds.parent(top_skeleton_nodes, 'bind')
 
         logger.info("build -- complete")
 
@@ -132,22 +174,45 @@ class Builder(object):
             cmpt._optimize_cmpt()
         logger.info("optimize -- complete")
 
-    def load_controlShapes(self, applyColor=True):
+    def load_controlShapes(self, path=None, applyColor=True):
         """
         Load the control shapes
+        :param path: path to control shape
         :param applyColor: Apply the control colors.
         :return:
         """
         import rigamajig2.maya.data.curve_data as curve_data
 
+        if path and os.path.exists(path=path):
+            control_shape_path = path
+        elif os.path.exists(self.absPath(self.get_rig_data(self.rig_file, CONTROL_SHAPES))):
+            control_shape_path = self.absPath(self.get_rig_data(self.rig_file, CONTROL_SHAPES))
+        else:
+            return
+
         cd = curve_data.CurveData()
-        if self.absPath(self.read_data(CONTROL_SHAPES)):
-            cd.read(self.absPath(self.read_data(CONTROL_SHAPES)))
+        cd.read(control_shape_path)
 
         controls = cd.getData().keys()
         logger.info("loading shapes for {} controls".format(len(controls)))
         cd.applyData(controls, applyColor=applyColor)
         logger.info("control shapes -- complete")
+
+    def save_controlShapes(self, path=None):
+        import rigamajig2.maya.data.curve_data as curve_data
+
+        if path:
+            control_shape_path = path
+        elif self.get_rig_data(self.rig_file, CONTROL_SHAPES):
+            control_shape_path = self.absPath(self.get_rig_data(self.rig_file, CONTROL_SHAPES))
+        else:
+            return
+
+        print control_shape_path
+        cd = curve_data.CurveData()
+        cd.gatherDataIterate(meta.getTagged("control"))
+        cd.write(control_shape_path)
+        logger.info("control shapes saved to: {}".format(control_shape_path))
 
     def load_data(self):
         """
@@ -226,6 +291,9 @@ class Builder(object):
 
     # UTILITY FUNCTIONS
     def set_rig_file(self, rigFile):
+        if not rigFile:
+            return
+
         if not os.path.exists(rigFile):
             # TODO: give the user the option to create a rig file somewhere
             raise RuntimeError("'{0}' does not exist".format(rigFile))
@@ -234,9 +302,12 @@ class Builder(object):
         rig_data = abstract_data.AbstractData()
         rig_data.read(self.rig_file)
         data = rig_data.getData()
-        if not data.has_key("rig_env"): rig_env_path = '../'
-        else: rig_env_path = data["rig_env"]
+        if not data.has_key("rig_env"):
+            rig_env_path = '../'
+        else:
+            rig_env_path = data["rig_env"]
         self.path = os.path.abspath(os.path.join(self.rig_file, rig_env_path))
+        logger.info('Rig Enviornment path: {0}'.format(self.path))
 
     def get_path(self):
         return self.path
@@ -262,19 +333,22 @@ class Builder(object):
         for cmpt in cmpts:
             self.cmpts.append(cmpt)
 
-    def read_data(self, key):
+    @classmethod
+    def get_rig_data(cls, rig_file, key):
         """
         read the data from the self.rig_file
+        :param rig_file:
         :param key:
         :return:
         """
-        if not os.path.exists(self.path):
-            raise RuntimeError('rig file at {} does not exist'.format(self.rig_file))
+        if not os.path.exists(rig_file):
+            raise RuntimeError('rig file at {} does not exist'.format(rig_file))
 
         data = abstract_data.AbstractData()
-        data.read(self.rig_file)
+        data.read(rig_file)
         if data.getData().has_key(key):
             return data.getData()[key]
+        return None
 
 
 def build_directory():

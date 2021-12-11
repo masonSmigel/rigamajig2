3f92bfc646b3fc7d98f6f21c48b8b8314685f1c8
masonSmigel <mgsmigel@gmail.com>
2021-12-11 01:36:01 -0500

    initial commit

--- scripts/rigamajig2/maya/cmpts/leg.py
+++ scripts/rigamajig2/maya/cmpts/leg.py
@@ -32,22 +32,34 @@ class Leg(rigamajig2.maya.cmpts.limb.Limb):
         :type pvSpaces: dict
         """
         super(Leg, self).__init__(name, input=input, size=size, ikSpaces=ikSpaces, pvSpaces=pvSpaces)
-
+        self.cmptSettings['toes_fkName'] = 'toes_fk'
         # noinspection PyTypeChecker
-        if len(self.input) != 4:
-            raise RuntimeError('Input list must have a length of 4')
+        if len(self.input) != 6:
+            raise RuntimeError('Input list must have a length of 6')
 
     def setInitalData(self):
-        self.cmptSettings['ikSpaces']['shoulder'] = self.cmptSettings['limbSwingName'] + '_' + self.side
-        self.cmptSettings['pvSpaces']['hand'] = self.cmptSettings['limb_ikName'] + '_' + self.side
+        self.cmptSettings['ikSpaces']['hip'] = self.cmptSettings['limbSwingName'] + '_' + self.side
+        self.cmptSettings['pvSpaces']['foot'] = self.cmptSettings['limb_ikName'] + '_' + self.side
 
     def initalHierachy(self):
         """Build the initial hirarchy"""
         super(Leg, self).initalHierachy()
+        return
+        # self.toes_fk = rig_control.createAtObject(self._userSettings['toes_fkName'], self.side,
+        #                                           hierarchy=['trsBuffer'],
+        #                                           hideAttrs=['v', 't', 's'], size=self.size, color='blue',
+        #                                           parent=self.control, shape='square', shapeAim='x',
+        #                                           xformObj=self.input[4])
+
+        # TODO: build foot roll piviots
+
+        # TODO: save piviot positions to the settings node
 
     def rigSetup(self):
         """Add the rig setup"""
         super(Leg, self).rigSetup()
+        # cmds.parentConstraint(self.toes_fk[-1], self.input[4])
+        # TODO: setup the foot roll
 
     def postRigSetup(self):
         """ Connect the blend chain to the bind chain"""

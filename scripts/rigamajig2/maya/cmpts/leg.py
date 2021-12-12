6e5b655f342b6c5bf89b3eda619de57249e52de3
masonSmigel <mgsmigel@gmail.com>
2021-12-12 18:10:20 -0500

    inital commit of ikfkFoot in leg. still to add controls

--- scripts/rigamajig2/maya/cmpts/leg.py
+++ scripts/rigamajig2/maya/cmpts/leg.py
@@ -41,6 +41,15 @@ class Leg(rigamajig2.maya.cmpts.limb.Limb):
         self.cmptSettings['ikSpaces']['hip'] = self.cmptSettings['limbSwingName'] + '_' + self.side
         self.cmptSettings['pvSpaces']['foot'] = self.cmptSettings['limb_ikName'] + '_' + self.side
 
+    def createBuildGuides(self):
+        """ create build guides """
+        self.guides = cmds.createNode("transform", name='{}_guide'.format(self.name))
+        self._heel_piv = cmds.spaceLocator(name="{}_heel".format(self.name))[0]
+        self._inn_piv = cmds.spaceLocator(name="{}_inn".format(self.name))[0]
+        self._out_piv = cmds.spaceLocator(name="{}_out".format(self.name))[0]
+
+        cmds.parent(self._heel_piv, self._inn_piv, self._out_piv, self.guides)
+
     def initalHierachy(self):
         """Build the initial hirarchy"""
         super(Leg, self).initalHierachy()
@@ -51,19 +60,36 @@ class Leg(rigamajig2.maya.cmpts.limb.Limb):
         #                                           parent=self.control, shape='square', shapeAim='x',
         #                                           xformObj=self.input[4])
 
-        # TODO: build foot roll piviots
-
-        # TODO: save piviot positions to the settings node
+        # TODO: create controls for the ik piviots
 
     def rigSetup(self):
         """Add the rig setup"""
         super(Leg, self).rigSetup()
         # cmds.parentConstraint(self.toes_fk[-1], self.input[4])
-        # TODO: setup the foot roll
+
+        self.footikfk = ikfk.IkFkFoot(jointList=self.input[3:], heelPivot=self._heel_piv, innPivot=self._inn_piv, outPivot=self._out_piv)
+        self.footikfk.setGroup(self.ikfk.getGroup())
+        self.footikfk.create()
+        ikfk.IkFkFoot.createFootRoll(self.footikfk.getPivotList(), self.footikfk.getGroup())
+
+        cmds.parent(self._ikEndTgt, self.footikfk.getPivotList()[-2])
+        cmds.parent(self.footikfk.getPivotList()[0], self.limbGimble_ik[-1])
+        cmds.delete(cmds.listRelatives(self._ikEndTgt, ad=True, type='pointConstraint'))
+
+        # Delete the proxy guides:
+        cmds.delete(self.guides)
 
     def postRigSetup(self):
         """ Connect the blend chain to the bind chain"""
-        super(Leg, self).postRigSetup()
+        blendedJointlist = self.ikfk.getBlendJointList() + self.footikfk.getBlendJointList()[1:]
+        print blendedJointlist
+        rigamajig2.maya.skeleton.connectChains(blendedJointlist, self.input[1:])
+        ikfk.IkFkBase.connnectIkFkVisibility(self.ikfk.getGroup(), 'ikfk', ikList=self.ikControls,
+                                             fkList=self.fkControls)
+
+        # connect the base to the main bind chain
+        cmds.parentConstraint(self.limbBase[-1], self.input[0])
+        rigamajig2.maya.attr.lock(self.input[0], rigamajig2.maya.attr.TRANSFORMS + ['v'])
 
     def connect(self):
         """Create the connection"""
@@ -72,3 +98,15 @@ class Leg(rigamajig2.maya.cmpts.limb.Limb):
     def finalize(self):
         """ Lock some attributes we dont want to see"""
         super(Leg, self).finalize()
+
+    def setupProxyAttributes(self):
+        """Setup the proxy attributes """
+        super(Leg, self).setupProxyAttributes()
+        rigamajig2.maya.attr.addSeparator(self.limb_ik[-1], '----')
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'roll'), self.limb_ik[-1])
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'bank'), self.limb_ik[-1])
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'heelSwivel'), self.limb_ik[-1])
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'toeSwivel'), self.limb_ik[-1])
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'toeTap'), self.limb_ik[-1])
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'ballAngle'), self.limb_ik[-1])
+        rigamajig2.maya.attr.addProxy('{}.{}'.format(self.ikfk.getGroup(), 'toeStraightAngle'), self.limb_ik[-1])

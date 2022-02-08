7e59e0314637ace8a013f1d0112a782f2ccc9811
masonSmigel <mgsmigel@gmail.com>
2022-02-08 13:24:05 -0500

    simplify build

--- tests/test_basic_build.py
+++ tests/test_basic_build.py
@@ -1,28 +1,21 @@
 import os
-import rigamajig2.maya.data.curve_data as curve_data
+
+import maya.app.evaluationToolkit.evaluationToolkit as et
 import maya.cmds as cmds
 
 from rigamajig2.maya.test.mayaunittest import TestCase
-
 import rigamajig2.maya.rig.builder as buider
 
 
 class TestBasicRigBuild(TestCase):
 
-    def test_simple_biped(self):
+    def test_biped_build(self):
         """
         build a simple biped rig
         """
-
-        self.load_plugin('quatNodes')
-        self.load_plugin('matrixNodes')
-
         archetype_path = (os.path.abspath(os.path.join(os.path.dirname(__file__),"../archetypes/biped/biped.rig")))
 
         b = buider.Builder(archetype_path)
         b.run()
 
         self.assertTrue(True)
-
-
-

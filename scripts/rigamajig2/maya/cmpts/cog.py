fda15b4ffc9bef78645288e5b951990439b3c49f
masonSmigel <mgsmigel@gmail.com>
2022-04-08 09:35:33 -0400

    added control shape options

--- scripts/rigamajig2/maya/cmpts/cog.py
+++ scripts/rigamajig2/maya/cmpts/cog.py
@@ -7,7 +7,8 @@ import rigamajig2.maya.rig.control as rig_control
 import rigamajig2.maya.rig.spaces as spaces
 import rigamajig2.maya.rig.ikfk as ikfk
 import rigamajig2.maya.transform as rig_transform
-import rigamajig2.shared.common as common
+import rigamajig2.maya.hierarchy as hierarchy
+import rigamajig2.maya.joint as joint
 import rigamajig2.maya.meta as meta
 import rigamajig2.maya.constrain as constrain
 
@@ -22,6 +23,8 @@ class Cog(rigamajig2.maya.cmpts.base.Base):
     def __init__(self, name, input=[], size=1):
         super(Cog, self).__init__(name, input=input, size=size)
 
+        self.cmptSettings['bind_to_input'] = False
+        self.cmptSettings['cog_control_shape'] = 'cube'
         self.cmptSettings['cog_name'] = 'hips'
         self.cmptSettings['cogGimble_name'] = 'hipsGimble'
         self.cmptSettings['cogPivot_name'] = 'hips_pivot'
@@ -40,7 +43,7 @@ class Cog(rigamajig2.maya.cmpts.base.Base):
         self.cog = rig_control.create(self.cog_name,
                                       hierarchy=['trsBuffer'],
                                       hideAttrs=['s', 'v'], size=self.size, color='yellow',
-                                      parent=self.control_hrc, shape='cube', shapeAim='x',
+                                      parent=self.control_hrc, shape=self.cog_control_shape, shapeAim='x',
                                       position=pos)
         self.cog_pivot = rig_control.create(self.cogPivot_name,
                                             hierarchy=['trsBuffer'],
@@ -50,12 +53,16 @@ class Cog(rigamajig2.maya.cmpts.base.Base):
         self.cog_gimble = rig_control.create(self.cogGimble_name,
                                              hierarchy=['trsBuffer', 'neg'],
                                              hideAttrs=['s', 'v'], size=self.size, color='yellow',
-                                             parent=self.cog[-1], shape='cube', shapeAim='x',
+                                             parent=self.cog[-1], shape=self.cog_control_shape, shapeAim='x',
                                              position=pos)
 
     def rigSetup(self):
         # create the pivot negate
         constrain.negate(self.cog_pivot[-1], self.cog_gimble[1], t=True)
+        if self.bind_to_input and len(self.input) >= 1:
+            self.input_trs = hierarchy.create(self.cog_gimble[-1], ['{}_trs'.format(self.input[0])], above=False)[0]
+            rig_transform.matchTransform(self.input[0], self.input_trs)
+            joint.connectChains(self.input_trs, self.input[0])
 
     def connect(self):
 

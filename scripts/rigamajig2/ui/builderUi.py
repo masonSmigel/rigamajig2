f747b96951b991f1edfd0c8a6f0d12f1fa7c77fd
masonSmigel <mgsmigel@gmail.com>
2022-04-08 09:35:19 -0400

    updated

--- scripts/rigamajig2/ui/builderUi.py
+++ scripts/rigamajig2/ui/builderUi.py
@@ -70,6 +70,10 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
 
     def create_actions(self):
         # FILE
+        self.new_rig_file_action = QtWidgets.QAction("New Rig File", self)
+        self.new_rig_file_action.setIcon(QtGui.QIcon(":fileNew.png"))
+        self.new_rig_file_action.triggered.connect(self.create_rig_env)
+
         self.load_rig_file_action = QtWidgets.QAction("Load Rig File", self)
         self.load_rig_file_action.setIcon(QtGui.QIcon(":folder-open.png"))
         self.load_rig_file_action.triggered.connect(self.load_rig_file)
@@ -104,6 +108,7 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         self.main_menu = QtWidgets.QMenuBar()
 
         file_menu = self.main_menu.addMenu("File")
+        file_menu.addAction(self.new_rig_file_action)
         file_menu.addAction(self.load_rig_file_action)
         file_menu.addAction(self.save_rig_file_action)
         file_menu.addSeparator()
@@ -122,14 +127,11 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
     def create_widgets(self):
         self.rig_path_selector = pathSelector.PathSelector(cap='Select a Rig File', ff="Rig Files (*.rig)", fm=1)
 
-        self.create_from_archetype_btn = QtWidgets.QPushButton("Create from Archetype")
-        self.create_from_archetype_btn.setToolTip("Create a new rig enviornment from scratch")
+        self.create_new_rigenv = QtWidgets.QPushButton("New Rig Env")
+        self.create_new_rigenv.setToolTip("Create a new rig enviornment")
 
         self.asset_name_le = QtWidgets.QLineEdit()
         self.asset_name_le.setPlaceholderText("asset_name")
-
-        self.clone_rig_env_btn = QtWidgets.QPushButton("Clone Existing Rig")
-        self.create_from_archetype_btn.setToolTip("Create a new rig enviornment from an existing enviornment")
 
         # Pre- script section
         self.preScript_wdgt = collapseableWidget.CollapsibleWidget('Pre-Script', addCheckbox=True)
@@ -201,9 +203,13 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         # self.finalize_rig_btn = QtWidgets.QPushButton("Finalize")
         self.edit_build_btn = QtWidgets.QPushButton("Edit Build")
         self.edit_build_btn.setFixedWidth(80)
-        self.edit_build_btn.setFixedHeight(LARGE_BTN_HEIGHT)
+        # self.edit_build_btn.setFixedHeight(LARGE_BTN_HEIGHT)
+        self.load_ctls_on_build = QtWidgets.QCheckBox("Load Ctls")
+        self.load_ctls_on_build.setChecked(True)
+        self.load_ctls_on_build.setFixedWidth(80)
+
         self.complete_build_btn = QtWidgets.QPushButton("Build Rig")
-        self.complete_build_btn.setFixedHeight(LARGE_BTN_HEIGHT)
+        self.complete_build_btn.setFixedHeight(45)
 
         # Post - script section
         self.postScript_wdgt = collapseableWidget.CollapsibleWidget('Post-Script', addCheckbox=True)
@@ -272,18 +278,15 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         self.close_btn = QtWidgets.QPushButton("Close")
 
     def create_layouts(self):
-        rig_env_btn_layout = QtWidgets.QHBoxLayout()
-        rig_env_btn_layout.addWidget(self.create_from_archetype_btn)
-        rig_env_btn_layout.addWidget(self.clone_rig_env_btn)
 
         rig_char_name_layout = QtWidgets.QHBoxLayout()
         rig_char_name_layout.addWidget(QtWidgets.QLabel("Rig Name:"))
         rig_char_name_layout.addWidget(self.asset_name_le)
+        rig_char_name_layout.addWidget(self.create_new_rigenv)
 
         rig_env_layout = QtWidgets.QVBoxLayout()
         rig_env_layout.addWidget(self.rig_path_selector)
         rig_env_layout.addLayout(rig_char_name_layout)
-        rig_env_layout.addLayout(rig_env_btn_layout)
 
         # prescript
         self.preScript_wdgt.addWidget(self.preScript_scriptRunner)
@@ -370,11 +373,16 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         self.cmpt_wdgt.addWidget(self.guide_path_selector)
         self.cmpt_wdgt.addLayout(guide_load_layout)
         build_layout = QtWidgets.QHBoxLayout()
+
+        build_option_layout = QtWidgets.QVBoxLayout()
+        build_option_layout.addWidget(self.edit_build_btn)
+        build_option_layout.addWidget(self.load_ctls_on_build)
+
         # build_step_layout.addWidget(self.build_rig_btn)
         # build_step_layout.addWidget(self.connect_rig_btn)
         # build_step_layout.addWidget(self.finalize_rig_btn)
         self.cmpt_wdgt.addLayout(build_layout)
-        build_layout.addWidget(self.edit_build_btn)
+        build_layout.addLayout(build_option_layout)
         build_layout.addWidget(self.complete_build_btn)
 
         # Post Script
@@ -484,12 +492,12 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
 
     def create_connections(self):
         self.rig_path_selector.select_path_btn.clicked.connect(self.path_selector_load_rig_file)
-        self.create_from_archetype_btn.clicked.connect(self.create_rig_env)
-        self.clone_rig_env_btn.clicked.connect(self.clone_rig_env)
+        self.create_new_rigenv.clicked.connect(self.create_rig_env)
 
         self.preScript_scriptRunner.reload_scripts_btn.clicked.connect(self.reload_prescripts)
 
         self.import_model_btn.clicked.connect(self.import_model)
+        self.open_model_btn.clicked.connect(self.open_model)
 
         self.import_load_skeleton_btn.clicked.connect(self.import_and_load_skeleton)
         self.import_skeleton_btn.clicked.connect(self.import_skeleton)
@@ -505,12 +513,9 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         self.load_guides_btn.clicked.connect(self.load_guides)
         self.save_guides_btn.clicked.connect(self.save_guides)
         self.load_components_btn.clicked.connect(self.load_components)
+        self.cmpt_manager.save_cmpt_btn.clicked.connect(self.save_components)
         self.cmpt_manager.clear_cmpt_btn.clicked.connect(self.clear_components)
         self.initalize_build_btn.clicked.connect(self.initalize_rig)
-
-        # self.build_rig_btn.clicked.connect(self.build_rig)
-        # self.connect_rig_btn.clicked.connect(self.connect_rig)
-        # self.finalize_rig_btn.clicked.connect(self.finalize_rig)
         self.edit_build_btn.clicked.connect(self.edit_build)
         self.complete_build_btn.clicked.connect(self.complete_build)
 
@@ -552,7 +557,7 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         data.read(self.rig_file)
         new_data = data.getData()
 
-        # TODO: Keep this updated as we add stuff to the builder
+        # Save the main feilds
         new_data['rig_name'] = self.asset_name_le.text()
         new_data['pre_script'] = self.preScript_scriptRunner.get_current_script_list(relative_paths=True)
         new_data['post_script'] = self.postScript_scriptRunner.get_current_script_list(relative_paths=True)
@@ -569,6 +574,7 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
 
         data.setData(new_data)
         data.write(self.rig_file)
+        logger.info("data saved to : {}".format(self.rig_file))
 
     def reload_rig_file(self):
         self.set_rig_file(self.rig_file)
@@ -660,10 +666,9 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
 
     # UTILITIY FUNCTIONS
     def create_rig_env(self):
-        print("TODO : create a rig environment")
-
-    def clone_rig_env(self):
-        print("TODO : clone a rig environment")
+        create_dialog = CreateRigEnvDialog()
+        create_dialog.new_env_created.connect(self.set_rig_file)
+        create_dialog.show_dialog()
 
     # CONNECTIONS
     def mirror_joint(self):
@@ -696,6 +701,9 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
     # BULDER FUNCTIONS
     def import_model(self):
         self.rig_builder.import_model(self.model_path_selector.get_abs_path())
+
+    def open_model(self):
+        cmds.file(self.model_path_selector.get_abs_path(), o=True, f=True)
 
     def import_and_load_skeleton(self):
         self.import_skeleton()
@@ -731,8 +739,13 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         self.rig_builder.load_component_settings(self.cmpt_path_selector.get_abs_path())
         self.cmpt_manager.load_list_from_builder()
 
+    def save_components(self):
+        self.rig_builder.load_meta_settings()
+        self.rig_builder.save_components(self.cmpt_path_selector.get_abs_path())
+
     def initalize_rig(self):
         self.rig_builder.initalize()
+        self.rig_builder.load_component_settings()
         self.cmpt_manager.load_cmpts_from_scene()
 
     def clear_components(self):
@@ -752,9 +765,12 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
 
     def complete_build(self):
         self.rig_builder.initalize()
+        self.rig_builder.load_component_settings()
         self.rig_builder.build()
         self.rig_builder.connect()
         self.rig_builder.finalize()
+        if self.load_ctls_on_build.isChecked():
+            self.load_controlShapes()
         self.cmpt_manager.load_cmpts_from_scene()
 
     def edit_build(self):
@@ -838,9 +854,11 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         res = confirm_pub_msg.exec_()
 
         if res == QtWidgets.QMessageBox.Save:
-            self.run_all()
-            self.rig_builder.publish(self.out_path_selector.get_abs_path(), self.asset_name_le.text(),
-                                     self.out_file_type_cb.currentText())
+            outputfile = self.out_path_selector.get_abs_path()
+            assetName = self.asset_name_le.text()
+            fileType = self.out_file_type_cb.currentText()
+
+            self.rig_builder.run(publish=True, outputfile=outputfile, assetName=assetName, fileType=fileType)
 
     # TOOLS MENU
     def run_performace_test(self):
@@ -852,7 +870,126 @@ class RigamajigBuilderUi(QtWidgets.QDialog):
         rigamajig2.reloadModule(log=True)
 
 
+class CreateRigEnvDialog(QtWidgets.QDialog):
+    WINDOW_TITLE = "Create Rig Enviornment"
+
+    new_env_created = QtCore.Signal(str)
+
+    rig_file_result = None
+
+    def show_dialog(self):
+        self.exec_()
+
+    def __init__(self):
+        if sys.version_info.major < 3:
+            maya_main_window = wrapInstance(long(omui.MQtUtil.mainWindow()), QtWidgets.QWidget)
+        else:
+            maya_main_window = wrapInstance(int(omui.MQtUtil.mainWindow()), QtWidgets.QWidget)
+
+        super(CreateRigEnvDialog, self).__init__(maya_main_window)
+        self.rig_env = None
+
+        self.setWindowTitle(self.WINDOW_TITLE)
+        if cmds.about(ntOS=True):
+            self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+        elif cmds.about(macOS=True):
+            self.setProperty("saveWindowPref", True)
+            self.setWindowFlags(self.windowFlags() ^ QtCore.Qt.WindowContextHelpButtonHint)
+        self.setFixedSize(375, 135)
+
+        self.create_widgets()
+        self.create_layouts()
+        self.create_connections()
+        self.update_create_method()
+
+    def create_widgets(self):
+
+        self.from_archetype_rb = QtWidgets.QRadioButton("New From Archetype")
+        self.from_existing_rb = QtWidgets.QRadioButton("Clone Existing")
+        self.from_archetype_rb.setChecked(True)
+
+        self.archetype_cb_widget = QtWidgets.QWidget()
+        self.archetype_cb_widget.setFixedHeight(25)
+        self.archetype_cb = QtWidgets.QComboBox()
+        for archetype in builder.get_available_archetypes():
+            self.archetype_cb.addItem(archetype)
+
+        self.src_path = pathSelector.PathSelector("Source:", fm=2)
+        self.dst_path = pathSelector.PathSelector("New Env:", fm=2)
+        self.rig_name_le = QtWidgets.QLineEdit()
+        self.rig_name_le.setPlaceholderText("rig_name")
+
+        self.create_btn = QtWidgets.QPushButton("Create")
+        self.cancel_btn = QtWidgets.QPushButton("Cancel")
+
+    def create_layouts(self):
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.setContentsMargins(6, 6, 6, 6)
+        main_layout.setSpacing(4)
+
+        radio_button_layout = QtWidgets.QHBoxLayout()
+        radio_button_layout.addSpacing(15)
+        radio_button_layout.addWidget(self.from_archetype_rb)
+        radio_button_layout.addWidget(self.from_existing_rb)
+
+        archetype_cb_layout = QtWidgets.QHBoxLayout(self.archetype_cb_widget)
+        archetype_cb_layout.setContentsMargins(0, 0, 0, 0)
+        label = QtWidgets.QLabel("Archetype:")
+        label.setFixedWidth(60)
+        archetype_cb_layout.addWidget(label)
+        archetype_cb_layout.addWidget(self.archetype_cb)
+
+        rig_name_layout = QtWidgets.QHBoxLayout()
+        label = QtWidgets.QLabel("Rig Name:")
+        label.setFixedWidth(60)
+        rig_name_layout.addWidget(label)
+        rig_name_layout.addWidget(self.rig_name_le)
+
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(self.cancel_btn)
+        button_layout.addWidget(self.create_btn)
+
+        main_layout.addLayout(radio_button_layout)
+        main_layout.addWidget(self.archetype_cb_widget)
+        main_layout.addWidget(self.src_path)
+        main_layout.addWidget(self.dst_path)
+        main_layout.addLayout(rig_name_layout)
+        main_layout.addLayout(button_layout)
+
+    def create_connections(self):
+        self.from_archetype_rb.toggled.connect(self.update_create_method)
+        self.from_existing_rb.toggled.connect(self.update_create_method)
+
+        self.cancel_btn.clicked.connect(self.close)
+        self.create_btn.clicked.connect(self.create_new_rig_env)
+
+    def update_create_method(self):
+        if self.from_archetype_rb.isChecked():
+            self.archetype_cb_widget.setVisible(True)
+            self.src_path.setVisible(False)
+        else:
+            self.archetype_cb_widget.setVisible(False)
+            self.src_path.setVisible(True)
+
+    def create_new_rig_env(self):
+
+        dest_rig_env = self.dst_path.get_path()
+        rig_name = self.rig_name_le.text()
+        if self.from_archetype_rb.isChecked():
+            archetype = self.archetype_cb.currentText()
+            rig_file = builder.new_rigenv_from_archetype(new_env=dest_rig_env, archetype=archetype, rig_name=rig_name)
+        else:
+            src_env = self.src_path.get_path()
+            rig_file = builder.create_rig_env(src_env=src_env, tgt_env=dest_rig_env, rig_name=rig_name)
+        self.new_env_created.emit(rig_file)
+
+        print "rig file ", rig_file
+        self.close()
+
+
 if __name__ == '__main__':
+
     try:
         rigamajig_builder_dialog.close()
         rigamajig_builder_dialog.deleteLater()

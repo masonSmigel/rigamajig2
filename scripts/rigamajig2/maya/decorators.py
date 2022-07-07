4b8596c261d0f270c447b0be481e88ec21b68c65
masonSmigel <mgsmigel@gmail.com>
2022-07-07 15:40:49 -0400

    created decorators module

---
+++ scripts/rigamajig2/maya/decorators.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    project: rigamajig2
+    file: decorators.py
+    author: masonsmigel
+    date: 07/2022
+
+"""
+from functools import wraps
+
+from maya import cmds as cmds
+
+
+def preserveSelection(func):
+    """
+    Decorator to preserve the selection before running a function
+    :param func: funtion to wrap
+    :return:
+    """
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        selection = cmds.ls(sl=True)
+
+        try:
+            return func(*args, **kwargs)
+
+        except Exception as e:
+            raise e
+
+        finally:
+            if selection:
+                cmds.select(selection)
+
+    return wrap
+
+
+def oneUndo(func):
+    """
+    Wrap the function with an open and close undo chunk.
+    :param func:
+    :return:
+    """
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        cmds.undoInfo(openChunk=True, chunkName=func.__name__)
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            raise e
+        finally:
+            cmds.undoInfo(closeChunk=True, chunkName=func.__name__)
+
+    return wrap

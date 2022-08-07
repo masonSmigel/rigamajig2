1b76378e5d99b6be78bff0f54acc5dfa87e1c14b
masonSmigel <mgsmigel@gmail.com>
2022-08-07 05:35:06 -0400

    added disable logging context manager

--- scripts/rigamajig2/shared/logger.py
+++ scripts/rigamajig2/shared/logger.py
@@ -11,6 +11,14 @@ import sys
 import logging
 
 
+class DisableLogger():
+    def __enter__(self):
+        logging.disable(logging.CRITICAL)
+
+    def __exit__(self, exit_type, exit_value, exit_traceback):
+        logging.disable(logging.NOTSET)
+
+
 # Custom Logger class
 class Logger(object):
     """

2a44d09651a217931fbf89291472271ceedd6b4a
masonSmigel <mgsmigel@gmail.com>
2022-11-09 13:02:11 -0500

    turned off the bendies by default in the biped template

--- archetypes/biped/post_scripts/setAttrs.py
+++ archetypes/biped/post_scripts/setAttrs.py
@@ -1,3 +1,9 @@
 import maya.cmds as cmds 
 
 cmds.setAttr("neck.tangentVis", 0)
+
+
+cmds.setAttr("arm_l.bendies", 0)
+cmds.setAttr("arm_r.bendies", 0)
+cmds.setAttr("leg_l.bendies", 0)
+cmds.setAttr("leg_r.bendies", 0)

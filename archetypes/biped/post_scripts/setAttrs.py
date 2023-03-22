2ce7cc5b368392f51cfff8748fe051418f76f572
masonSmigel <mgsmigel@gmail.com>
2023-03-21 21:31:12 -0400

    tmp commit to fix scripts running in reverse order

--- archetypes/biped/post_scripts/setAttrs.py
+++
@@ -1,9 +0,0 @@
-import maya.cmds as cmds 
-
-cmds.setAttr("neck.tangentVis", 0)
-
-
-cmds.setAttr("arm_l.bendies", 0)
-cmds.setAttr("arm_r.bendies", 0)
-cmds.setAttr("leg_l.bendies", 0)
-cmds.setAttr("leg_r.bendies", 0)

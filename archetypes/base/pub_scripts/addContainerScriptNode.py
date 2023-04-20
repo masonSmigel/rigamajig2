2126dc3d1ca954b9defa7f70e7284bb447a58b73
masonSmigel <mgsmigel@gmail.com>
2023-04-20 12:25:42 -0400

    added a check to see if the scene config exists

--- archetypes/base/pub_scripts/addContainerScriptNode.py
+++ archetypes/base/pub_scripts/addContainerScriptNode.py
@@ -2,4 +2,6 @@ import rigamajig2.maya.container as continer
 import rigamajig2.maya.scriptNode as scriptNode
 import maya.cmds as cmds 
 
-scriptNode.create('rigamajig2_sceneConfig', scriptType='Open/Close', beforeScript = continer.sainityCheck)
+# check if the riamajig scene config exists. If it doesnt create it 
+if not cmds.objExists('rigamajig2_sceneConfig'):
+    scriptNode.create('rigamajig2_sceneConfig', scriptType='Open/Close', beforeScript=continer.sainityCheck)

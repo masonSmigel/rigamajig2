480d3a824d7a18b57602477f520fa59b7e112052
masonSmigel <mgsmigel@gmail.com>
2023-04-20 11:09:31 -0400

    updated component versions

--- scripts/rigamajig2/maya/cmpts/lookAt/eyeballs.py
+++ scripts/rigamajig2/maya/cmpts/lookAt/eyeballs.py
@@ -31,7 +31,7 @@ class Eyeballs(rigamajig2.maya.cmpts.lookAt.lookAt.LookAt):
     """
     VERSION_MAJOR = 1
     VERSION_MINOR = 0
-    VERSION_PATCH = 0
+    VERSION_PATCH = 1
 
     version_info = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
     version = '%i.%i.%i' % version_info

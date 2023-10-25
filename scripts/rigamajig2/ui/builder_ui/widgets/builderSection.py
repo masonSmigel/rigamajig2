3e256508ec27fe366b4b4ecb51349aee76561694
masonSmigel <mgsmigel@gmail.com>
2023-10-25 18:18:03 -0400

    added a run widget button to the header

--- scripts/rigamajig2/ui/builder_ui/widgets/builderSection.py
+++ scripts/rigamajig2/ui/builder_ui/widgets/builderSection.py
@@ -9,10 +9,74 @@
 
 """
 from PySide2 import QtCore
+from PySide2 import QtGui
 from PySide2 import QtWidgets
 
 # RIGAMAJIG2
-from rigamajig2.ui.builder_ui.widgets import builderHeader
+from rigamajig2.shared import common
+from rigamajig2.ui.widgets import collapseableWidget
+
+breakpointStylesheet = (f"""
+                    QCheckBox {{
+                        spacing: 5px;
+                    }}
+
+                    QCheckBox::indicator {{
+                        width: 13px;
+                        height: 13px;
+                    }}
+
+                    QCheckBox::indicator:unchecked {{
+                        image: url({common.ICONS_PATH}/breakpoint_unchecked.png);
+                    }}
+
+                    QCheckBox::indicator:unchecked:hover {{
+                        image: url({common.ICONS_PATH}/breakpoint_hover.png);
+                    }}
+
+                    QCheckBox::indicator:unchecked:pressed {{
+                        image: url({common.ICONS_PATH}/breakpoint_checkedPress.png);
+                    }}
+
+                    QCheckBox::indicator:checked {{
+                        image: url({common.ICONS_PATH}/breakpoint_checked.png);
+                    }}
+
+                    QCheckBox::indicator:checked:hover {{
+                        image: url({common.ICONS_PATH}/breakpoint_checked.png);
+                    }}
+
+                    QCheckBox::indicator:checked:pressed {{
+                        image: url({common.ICONS_PATH}/breakpoint_checkedPress.png);
+                    }}
+                    """)
+
+
+class BuilderHeader(collapseableWidget.CollapsibleWidget):
+    headerCheckedColor = QtGui.QColor(78, 116, 125)
+    headerDefaultColor = QtWidgets.QPushButton().palette().color(QtGui.QPalette.Button)
+
+    def __init__(self, text, parent=None, addCheckbox=True):
+        super(BuilderHeader, self).__init__(text, parent, addCheckbox)
+
+        # set the check of the header to set-checked method.
+        self.headerWidget.checkbox.clicked.connect(self.setChecked)
+        if self.headerWidget.hasCheckBox:
+            self.headerWidget.checkbox.setStyleSheet(breakpointStylesheet)
+
+        self.runWidgetButton = QtWidgets.QPushButton()
+        self.runWidgetButton.setIcon(QtGui.QIcon(":timestart.png"))
+        self.runWidgetButton.setFlat(True)
+        self.runWidgetButton.setFixedSize(QtCore.QSize(16, 16))
+
+        self.headerWidget.mainLayout.addWidget(self.runWidgetButton)
+
+    def setChecked(self, checked):
+        super(BuilderHeader, self).setChecked(checked)
+
+        color = self.headerCheckedColor if checked else self.headerDefaultColor
+        self.setHeaderBackground(color=color)
+
 
 
 class BuilderSection(QtWidgets.QWidget):
@@ -27,12 +91,14 @@ class BuilderSection(QtWidgets.QWidget):
         self.builder = None
         self.rigEnvironment = None
 
-        self.mainWidget = builderHeader.BuilderHeader(text=self.WIDGET_TITLE, addCheckbox=True)
+        self.mainWidget = BuilderHeader(text=self.WIDGET_TITLE, addCheckbox=True)
         self.mainLayout = QtWidgets.QVBoxLayout(self)
         self.mainLayout.setContentsMargins(0, 0, 0, 0)
         self.mainLayout.setSpacing(0)
 
         self.mainLayout.addWidget(self.mainWidget)
+
+        self.mainWidget.runWidgetButton.clicked.connect(self._runWidget)
 
         self.createWidgets()
         self.createLayouts()
